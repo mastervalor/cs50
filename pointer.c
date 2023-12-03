49fcc622a43c4c88b4a36035246922ee0a9d32f0
mastervalor <mouradmarzouk63@gmail.com>
2023-12-02 22:01:04 -0800

    updae pointer

--- pointer.c
+++ pointer.c
@@ -11,4 +11,11 @@ int main(void)
     printf("pointer is %p\n", p);
     //to go and get the value from the pointer is poting to
     printf("pointer is %i\n", *p);
+
+
+    //you can also do pointer arithmetic, when telling it to use a pointer you can add to the memory int location to get the next part in memory
+    char *s = "HI!";
+    printf("The pointer of s is %c\n", *s);
+    printf("The pointer of s is %c\n", *(s+1));
+    printf("The pointer of s is %c\n", *(s+1));
 }
--- strings-in-c.c
+++ strings-in-c.c
@@ -8,4 +8,10 @@ int main(void)
     //printf is smart enought to know the difference in what you are tring to bring by the % and s can be printed as a string or a pointer
     printf("The pointer of s is %p\n", s);
     printf("The pointer of s is %s\n", s);
+
+    //to get the address of each character in the string, the & is to get the turn that varaibe into looking into its pointer
+    printf("The pointer of s is %p\n", &s[0]);
+    printf("The pointer of s is %p\n", &s[1]);
+    printf("The pointer of s is %p\n", &s[2]);
+    printf("The pointer of s is %p\n", &s[3]);
 }
