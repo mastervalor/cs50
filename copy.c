4535c7d0408405fabc70f4e76429483101f26857
mastervalor <mouradmarzouk63@gmail.com>
2023-12-03 18:27:39 -0800

    Update copy.c

--- copy.c
+++ copy.c
@@ -6,20 +6,30 @@
 int mani (void)
 {
     char *s = get_string("s: ");
+    //get_string and other fucntions can return a Null value to tell you something was wrong, so its a good idea to catch this usecase
+    if (s == NULL);
+    {
+        return 1;
+    }
 
     //malloc function tells the computer to give me however much I'm asking for in memory dedicated for that variable
     char *t = malloc(strlen(s) + 1);
 
     //now I need to loop through it to get the all the chars added to the new string, the +1 is to make sure you copy the null char too thats outside the size of the string 
     //by design you don't want to call a fucntion over and over again in your loop so better to set a variable for it and compare to that 
-    for (int i = 0, n = strlen(s) + 1; i < n; i++)
-    {
-        t[i] = s[i];
-    }
+    // for (int i = 0, n = strlen(s) + 1; i < n; i++)
+    // {
+    //     t[i] = s[i];
+    // }
+
+    //strcpy does the same thing as that for loop
+    strcpy(t,s);
 
     //if I do this without malloc it would point to the same spot in memory t[0] shares with s[0] and would make the s[0] cappital too.
-    t[0] = toupper(t[0]);
-
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
 
     printf("s: %s\n", s);
      printf("t: %s\n", t);
