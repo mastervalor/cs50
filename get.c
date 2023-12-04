35cf91598fc3e245693987d9777e8e39eb5adf68
mastervalor <mouradmarzouk63@gmail.com>
2023-12-03 19:58:03 -0800

    how to get input from users and how to open files in C

---
+++ get.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    printf("x: ");
+    scanf("%i", &x);
+    printf("x: %i\n", x);
+}
+
+int main(void)
+{
+    char s[4];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+}
