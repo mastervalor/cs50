7d7caba40644a8cf3bad00a7116c50da7d3dcd5f
mastervalor <mouradmarzouk63@gmail.com>
2023-12-03 19:33:25 -0800

    Create memory.c

---
+++ memory.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b);
+
+int main(void)
+{
+    int x = 1;
+    int y = 2;
+
+    printf("x is %i, y is %i\n", x, y);
+    swap(&x, &y);
+    printf("x is %i, y is %i\n", x, y);
+}
+
+
+//without the making a and b a pointer they will create thier own slot in memory, swap to them selfs and leave x and y as is
+//so you need to make them pointers so that they swap values on x and y instead of in their own memory.
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
